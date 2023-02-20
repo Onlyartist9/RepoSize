a14c314641930f771286c9a9ce4ba857a6141d4b
Samuel NTA Gyamfi <49637763+Onlyartist9@users.noreply.github.com>
2023-02-20 13:38:22 +0000

    Fixed subcommand issue.

--- RepoSize/model.py
+++ RepoSize/model.py
@@ -1,8 +1,5 @@
 import argparse
 
-#Making use of the argparse parser.
-parser =argparse.ArgumentParser(prog="reposize",description="Find the size of the specifed Github repository.",epilog="Thanks for using %(prog)s!")
-
 '''
 What we want to design is a CLI that takes in commands in a myriad of ways.
 The simple way simply accepts <owner>/<repo> and returns the size in a myriad of ways
@@ -13,11 +10,21 @@ the private subcommand which comes with a switch allowing for the
 addition of a authentication token. 
 
 We should also detail how a user may get an authentication token in a help section that can be activated when the file alone is used.
+
+command for basic query search should be simply
+
+General purpose query:
+python reposize <owner>/<repo>
+
+Private repo query
+python reposize private -t <token> <owner>/<repo>
 '''
 
+#Making use of the argparse parser.
+parser =argparse.ArgumentParser(prog="reposize",description="Find the size of the specifed Github repository.",epilog="Thanks for using %(prog)s!")
+subparser = parser.add_subparsers(title="subcommand", help ="In case you need to find the size of one of your own repos you'll need an authentication token.Once you have that though use this subcommand to enter the token and access it.")
 #Adding a subcommand
 parser.add_argument("repository")
-parser.add_argument("-r","--repo",action="store_true")
 
 arguments = parser.parse_args()
 target_repo = arguments.repository
--- RepoSize/reposize.py
+++ RepoSize/reposize.py
@@ -1,13 +1,18 @@
 import requests
 import model
-import json
 
-reporequest = model.target_repo
-
-def requester(reporequest):
+def requester(request):
+    """validate value of request value: should be user/repo
+    Probably making use of a regular expression. should probably look into if
+    Github has a specifc format for user/repo.
+    """
+    response = requests.get(
+        "https://api.github.com/repos/"+"request"
+    )
     #Handles requests and parses said request
+    json_response = response.json()
+    size = json_response['size']
     return size
-
 
 def displayer(size):
     if size >= 1000:
@@ -15,7 +20,9 @@ def displayer(size):
         print("Your size is %f MB",sizeinMB)
     elif size >= 10**6:
         sizeinGB = size/10**6
-        print("Your size is %f MB",sizeinGB)
+        print("Your size is %f GB",sizeinGB)
+
+reporequest = model.target_repo
 
 request = requester(reporequest)
 displayer(request)
