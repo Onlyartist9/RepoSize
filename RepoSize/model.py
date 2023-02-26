4e0e19d2d3ae2345d4d28c7c943ab8e06326d13c
Samuel NTA Gyamfi <49637763+Onlyartist9@users.noreply.github.com>
2023-02-26 15:55:26 +0000

    Added input validation.

--- RepoSize/model.py
+++ RepoSize/model.py
@@ -1,10 +1,24 @@
 import argparse
+import re
+import sys
 
 parser =argparse.ArgumentParser(prog="reposize",description="Find the size of the specifed Github repository. Used this way: python reposize lucidrains/toolformer-pytorch",epilog="Thanks for using %(prog)s!")
 
-parser.add_argument("<user/repo>")
-parser.add_argument("-v","--version",action="version",version="%(prog)s 1.0.0")
+parser.add_argument("repository")
+parser.add_argument("-v","--version",action="version",version="%(prog)s 1.1.0")
 
 arguments = parser.parse_args()
 target_repo = arguments.repository
+
+pattern = "^[a-zA-Z0-9_.-]+\/[a-zA-Z0-9_.-]+$"
+
+match = re.match(pattern, target_repo)
+try:
+    if not match:
+        raise ValueError("Invalid input")
+except ValueError as e:
+    print(e)
+    print("Please enter a valid Github repository in the form <user>/<repository>")
+    sys.exit(1)
+
 
